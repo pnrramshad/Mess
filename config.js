8d4a7a564c40466a28d0cb98e5a3b41e200ce8cb
pnrramshad <pnrramshad@gmail.com>
2025-10-08 11:34:36 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const GITHUB_OWNER = "pnrramshad";
+const GITHUB_REPO = "mess";
+const GITHUB_FILE = "data.json";
+const GITHUB_BRANCH = "main";
+const GITHUB_TOKEN = "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";
